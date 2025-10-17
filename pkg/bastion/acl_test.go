eeaadf491c6ea61cba2a7956c5b180cee187f1b2
libvoid <135131094+libvoid@users.noreply.github.com>
2025-10-17 19:23:40 +0200

    fix(test|ci): unit tests never running + refactor acl_test
    go test is not recursive by default so it wasn't checking test files in the bastion / utils / crypto packages

--- pkg/bastion/acl_test.go
+++ pkg/bastion/acl_test.go
@@ -1,47 +1,60 @@
 package bastion // import "moul.io/sshportal/pkg/bastion"
 
 import (
-	"io/ioutil"
 	"os"
+	"fmt"
 	"path/filepath"
 	"testing"
-
-	. "github.com/smartystreets/goconvey/convey"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"moul.io/sshportal/pkg/dbmodels"
 )
 
+func SetupBase(aesKey string) (*gorm.DB, string, error) {
+	tmpDir, err := os.MkdirTemp("", "sshportal")
+
+	if err != nil {
+		return nil, "", fmt.Errorf("Can't setup test: %v", err)
+	}
+	//
+
+	db, err := gorm.Open(sqlite.Open(filepath.Join(tmpDir, "sshportal.db")), &gorm.Config{})
+	if err != nil {
+		return nil, "", fmt.Errorf("Can't setup test DB: %v", err)
+	}
+
+	DBInit(db, aesKey)
+
+	return db, tmpDir, nil
+}
+
 func TestCheckACLs(t *testing.T) {
-	Convey("Testing CheckACLs", t, func(c C) {
-		// create tmp dir
-		tempDir, err := ioutil.TempDir("", "sshportal")
-		c.So(err, ShouldBeNil)
-		defer func() {
-			c.So(os.RemoveAll(tempDir), ShouldBeNil)
-		}()
+	db, tmpDir, err := SetupBase("")
+	defer os.RemoveAll(tmpDir)
 
-		// create sqlite db
-		db, err := gorm.Open(sqlite.Open(filepath.Join(tempDir, "sshportal.db")), &gorm.Config{})
-		c.So(err, ShouldBeNil)
-		c.So(DBInit(db, ""), ShouldBeNil)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	// create dummy objects
+	var hostGroup dbmodels.HostGroup
+	if err := dbmodels.HostGroupsByIdentifiers(db, []string{"default"}).First(&hostGroup).Error; err != nil {
+		t.Errorf("Can't create host")
+	}
 
-		// create dummy objects
-		var hostGroup dbmodels.HostGroup
-		err = dbmodels.HostGroupsByIdentifiers(db, []string{"default"}).First(&hostGroup).Error
-		c.So(err, ShouldBeNil)
-		db.Create(&dbmodels.Host{Groups: []*dbmodels.HostGroup{&hostGroup}})
+	if err := db.Create(&dbmodels.Host{Groups: []*dbmodels.HostGroup{&hostGroup}}).Error; err != nil {
+		t.Errorf("%v", err)
+	}
 
-		//. load db
-		var (
-			hosts []dbmodels.Host
-			users []dbmodels.User
-		)
-		db.Preload("Groups").Preload("Groups.ACLs").Find(&hosts)
-		db.Preload("Groups").Preload("Groups.ACLs").Find(&users)
+	var (
+		hosts []dbmodels.Host
+		users []dbmodels.User
+	)
+	db.Preload("Groups").Preload("Groups.ACLs").Find(&hosts)
+	db.Preload("Groups").Preload("Groups.ACLs").Find(&users)
 
-		// test
-		action := checkACLs(users[0], hosts[0], "")
-		c.So(action, ShouldEqual, dbmodels.ACLActionAllow)
-	})
+	// test
+	action := checkACLs(users[0], hosts[0], "")
+	if action != string(dbmodels.ACLActionAllow) {
+		t.Errorf("ACL test fail")
+	}
 }
