9924335feb7293e620b2983e984901f49c09d823
libvoid <135131094+libvoid@users.noreply.github.com>
2025-10-31 22:19:05 +0100

    fix(build): windows build failing since urfave/cli upgrade
    fix regression introduced in 80dd371

--- testserver_unsupported.go
+++ testserver_unsupported.go
@@ -3,12 +3,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/urfave/cli/v3"
 )
 
 // testServer is an hidden handler used for integration tests
-func testServer(c *cli.Context) error {
+func testServer(_ context.Context, _ *cli.Command) error {
 	return fmt.Errorf("not available on windows")
 }
